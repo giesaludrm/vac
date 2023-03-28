1284837dbb01719803af4e0875983b9af78de0f2
fportales <68240613+fportalesor@users.noreply.github.com>
2023-03-28 16:41:42 -0300

    Add files via upload

---
+++ Bienvenida.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Monitoreo Registro Regional de Inmunizaciones",
+    #page_icon="👋",
+)
+
+st.write("# Monitoreo Registro Regional de Inmunizaciones")
+
+st.markdown("""##### Fecha corte de datos: """ )
+
+st.markdown(
+    """
+    Esta plataforma presenta información con desagregación comunal...
+
+
+"""
+)
