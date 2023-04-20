a61d025695f4e478ea05cd958fcfd73b61d00307
fportales <68240613+fportalesor@users.noreply.github.com>
2023-04-20 14:49:32 -0400

    Add files via upload

--- Bienvenida.py
+++ Bienvenida.py
@@ -1,18 +1,34 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="Monitoreo Registro Regional de Inmunizaciones",
+    page_title="Monitoreo Regional del Registro Inmunizaciones",layout="wide"
     #page_icon="👋",
 )
+st.markdown("""<p style="text-align:center;"><img src="https://seremi13.redsalud.gob.cl/wrdprss_minsal/wp-content/uploads/2021/06/isologo2-2021.jpg"
+alt="whatever" width="120" height= "110"></p>""", unsafe_allow_html=True)
 
-st.write("# Monitoreo Registro Regional de Inmunizaciones")
+st.markdown("""<div style='text-align: center; color: black; font-size: 12px;'>
+                SEREMI de Salud Región Metropolitana <br>
+                Depto. de Salud Pública y Planificación Sanitaria <br>
+                Subdepto. Gestión de Información y Estadísticas <br> 
+                 </div>""", unsafe_allow_html=True)
 
-st.markdown("""##### Fecha corte de datos: """ )
-
-st.markdown(
-    """
-    Esta plataforma presenta información con desagregación comunal...
+#st.write("## Monitoreo Registro Regional de Inmunizaciones")
+st.markdown("<h1 style='text-align: center; color: black;'>Monitoreo Regional del Registro Inmunizaciones</h1>", unsafe_allow_html=True)
 
 
-"""
-)
+
+
+col1, col2, col3 = st.columns([1,2.5,1])
+
+with col2:
+    st.markdown(
+    """<div style="text-align: justify;"><br> Esta plataforma deja a disposición del sector Salud, información de alcance regional para
+    el monitoreo de vacunas del Programa Nacional de Inmunizaciones, siendo su objetivo principal el apoyo para la gestión, 
+    basándose en la utilización de datos provisorios, los cuales se presentan a modo de indicadores y gráficos con desagregación por:
+    Región Metropolitana, Servicios de Salud Metropolitanos, Comunas y Establecimientos de Salud. <br> <br>
+    La herramienta fue desarrollada mediante un trabajo colaborativo entre el Subdepto. de Gestión de Información
+    y Estadísticas, Subdepto. de Epidemiología y el equipo del Programa Nacional de Inmunizaciones de la SEREMI de
+    Salud Metropolitana.</div>""", unsafe_allow_html=True)
+
+
