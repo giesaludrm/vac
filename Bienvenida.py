5155ed23db591a565b31ab847e3eb8e5d708fd32
fportales <68240613+fportalesor@users.noreply.github.com>
2023-04-21 11:26:17 -0400

    Add files via upload

--- Bienvenida.py
+++ Bienvenida.py
@@ -4,6 +4,23 @@ st.set_page_config(
     page_title="Monitoreo Regional del Registro Inmunizaciones",layout="wide"
     #page_icon="👋",
 )
+def add_logo():
+    st.markdown(
+        """
+        <style>
+            [data-testid="stSidebarNav"] {
+                background-image: url(https://seremi13.redsalud.gob.cl/wrdprss_minsal/wp-content/uploads/2021/06/isologo2-2021.jpg);
+                background-repeat: no-repeat;
+                background-size: 130px 110px;
+                padding-top: 60px;
+                background-position: 50px 20px;
+            }
+        </style>
+        """,
+        unsafe_allow_html=True,
+    )
+add_logo()
+
 st.markdown("""<p style="text-align:center;"><img src="https://seremi13.redsalud.gob.cl/wrdprss_minsal/wp-content/uploads/2021/06/isologo2-2021.jpg"
 alt="whatever" width="120" height= "110"></p>""", unsafe_allow_html=True)
 
